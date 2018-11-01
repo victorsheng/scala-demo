065e3ce85bd9524db1904062a859dddc62986afb
victor <920443721@qq.com>
2018-11-01 16:18:08 +0800

    finsh lesson 1

---
+++ hello/readme.md
@@ -0,0 +1,13 @@
+# 中文教程
+## w3c1
+https://www.w3cschool.cn/scala/scala-index.html
+
+## w3c2
+https://www.w3cschool.cn/scala/85pl1jc9.html
+
+
+
+
+
+# 英文教程
+https://www.tutorialspoint.com/scala/index.htm
---
+++ hello/src/collection/ArrayDemo.scala
@@ -0,0 +1,16 @@
+package collection
+
+object ArrayDemo {
+
+  def main(args: Array[String]) {
+    var books = new Array[String](3)
+    books(0) = "Scala";
+    books(1) = "Java";
+    books(2) = "Groovy"
+    println(books(0))
+
+
+
+  }
+
+}
